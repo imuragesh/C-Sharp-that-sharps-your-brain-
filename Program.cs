06e20280525c045348b5495780ea80ce42430666
imuragesh <120395356+imuragesh@users.noreply.github.com>
2023-01-24 23:18:42 +0530

    overloads method example

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overLoadMethod
+{
+
+
+    internal class Program
+    {
+        public void test()
+        {
+            Console.WriteLine("first method");
+        }
+        public void test( int i)
+        {
+            Console.WriteLine("second method");
+        }
+        public void test(string s)
+        {
+            Console.WriteLine("third method");
+        }
+        public void test(int i ,string s)
+        {
+            Console.WriteLine("fourth method method");
+        }
+        public void test(string s,int i)
+        {
+            Console.WriteLine("fifth method");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.test();
+            p.test(10);
+            p.test("b");
+            p.test(2, "hi");
+            p.test("hi", 1);
+           
+        }
+    }
+}
